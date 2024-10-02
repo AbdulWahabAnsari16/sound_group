74fafa983ac5d327038f835ac5760b48e8f7c587
Abdul Wahab <abdulwahabb11023@gmail.com>
2024-10-02 17:55:01 +0500

    2-10-24

--- User_Panel/index.php
+++ User_Panel/index.php
@@ -10,6 +10,10 @@ if ($_SESSION['e']) {
 
 	$sqlSelect2 = "SELECT * FROM genre,artist WHERE  genre.art_id = artist.art_id ";
 	$res2 = mysqli_query($conn, $sqlSelect2);
+
+	$sqlSelect4 = "SELECT MIN(music.mus_id) AS mus_id, music.title, music.art_id, music.year, music.gen_id, music.lang_id, music.alb_id, music.mus_file, artist.art_name, artist.art_img FROM music JOIN artist ON music.art_id = artist.art_id GROUP BY artist.art_name ";
+	$res4 = mysqli_query($conn, $sqlSelect4);
+	$row4 = mysqli_fetch_array($res4);
 } else {
 	$sqlSelect1 = "SELECT * FROM `artist` limit 2";
 	$res1 = mysqli_query($conn, $sqlSelect1);
@@ -19,6 +23,20 @@ if ($_SESSION['e']) {
 	$res2 = mysqli_query($conn, $sqlSelect2);
 }
 ?>
+
+<div class="player">
+		<div class="player__cover">
+			<img src="img/covers/cover.svg" alt="">
+		</div>
+
+		<div class="player__content">
+			<span class="player__track"><b class="player__title">Epic Cinematic</b> – <span class="player__artist"><?php echo $row4['art_name']; ?></span></span>
+			<audio src="http://blast.volkovdesign.com/audio/12071151_epic-cinematic-trailer_by_audiopizza_preview.mp3" id="audio" controls></audio>
+		</div>
+	</div>
+
+	<button class="player__btn" type="button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.65,2.24a1,1,0,0,0-.8-.23l-13,2A1,1,0,0,0,7,5V15.35A3.45,3.45,0,0,0,5.5,15,3.5,3.5,0,1,0,9,18.5V10.86L20,9.17v4.18A3.45,3.45,0,0,0,18.5,13,3.5,3.5,0,1,0,22,16.5V3A1,1,0,0,0,21.65,2.24ZM5.5,20A1.5,1.5,0,1,1,7,18.5,1.5,1.5,0,0,1,5.5,20Zm13-2A1.5,1.5,0,1,1,20,16.5,1.5,1.5,0,0,1,18.5,18ZM20,7.14,9,8.83v-3L20,4.17Z"/></svg> Player</button>
+
 <!-- main content -->
 <main class="main">
 	<div class="container-fluid">
@@ -42,13 +60,19 @@ if ($_SESSION['e']) {
 			while ($row3 = mysqli_fetch_array($res3)) {
 			?>
 				<div class="col-6 col-sm-4 col-lg-2">
-					<div class="album">
-						<div class="album__cover">
+					<div class="album main__list main__list--playlist main__list--dashbox"">
+						<div class="album__cover single-item">
 							<img src="img/covers/cover.svg" alt="">
-
-							<a href="player.php?id=<?php echo $row3[0]; ?>"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+							<a data-playlist data-title="<?php echo $row3['title'] ?>" data-artist="<?php echo $row3['art_name'] ?>" data-img="img/covers/cover.svg" href="music/<?php echo $row3['mus_file']; ?>" id="audio"
+								class="single-item__cover">
+								<img src="img/covers/cover.svg" alt="">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 									<path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z" />
-								</svg></a>
+								</svg>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<path d="M16,2a3,3,0,0,0-3,3V19a3,3,0,0,0,6,0V5A3,3,0,0,0,16,2Zm1,17a1,1,0,0,1-2,0V5a1,1,0,0,1,2,0ZM8,2A3,3,0,0,0,5,5V19a3,3,0,0,0,6,0V5A3,3,0,0,0,8,2ZM9,19a1,1,0,0,1-2,0V5A1,1,0,0,1,9,5Z" />
+								</svg>
+							</a>
 						</div>
 						<div class="album__title">
 							<h3><a href="release.php"><?php echo $row3['title']; ?></a></h3>
